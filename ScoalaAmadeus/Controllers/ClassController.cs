601fb2745982f375bb12340b9654bcbc33bdac94
Nechita Ioel <ioelnechita@ymail.com>
2019-09-10 15:36:47 +0300

    Class : Controller, Repository and Views

---
+++ ScoalaAmadeus/Controllers/ClassController.cs
@@ -0,0 +1,110 @@
+﻿using ScoalaAmadeus.Models;
+using ScoalaAmadeus.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScoalaAmadeus.Controllers
+{
+    public class ClassController : Controller
+    {
+        private ClassRepository classRepository = new ClassRepository();
+        
+        // GET: Class
+        public ActionResult Index()
+        {
+            List<ClassModel> classesList = new List<ClassModel>();
+            classesList = classRepository.GetAllClasses();
+            return View("Index", classesList);
+        }
+
+        // GET: Class/Details/5
+        public ActionResult Details(Guid id)
+        {
+            ClassModel classModel = new ClassModel();
+            classModel = classRepository.GetClassById(id);
+            return View("Details", classModel);
+        }
+
+        // GET: Class/Create
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: Class/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ClassModel classModel = new ClassModel();
+
+                UpdateModel(classModel);
+
+                classRepository.Insert(classModel);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+
+        // GET: Class/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            ClassModel classModel = classRepository.GetClassById(id);
+            return View("Edit", classModel);
+        }
+
+        // POST: Class/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Guid id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                ClassModel classModel = new ClassModel();
+
+                UpdateModel(classModel);
+
+                classRepository.Update(classModel);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Edit");
+            }
+        }
+
+        // GET: Class/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            ClassModel classModel = classRepository.GetClassById(id);
+            return View("Delete", classModel);
+        }
+
+        // POST: Class/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Guid id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                classRepository.Delete(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Delete");
+            }
+        }
+    }
+}
---
+++ ScoalaAmadeus/Repository/ClassRepository.cs
@@ -0,0 +1,85 @@
+﻿using ScoalaAmadeus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoalaAmadeus.Repository
+{
+    public class ClassRepository
+    {
+        private Models.DBObjects.SchoolsModelsDataContext dbContext;
+
+        public ClassRepository()
+        {
+            dbContext = new Models.DBObjects.SchoolsModelsDataContext();
+        }
+        public ClassRepository(Models.DBObjects.SchoolsModelsDataContext dataContext)
+        {
+            dbContext = dataContext;
+        }
+
+        public void Insert(ClassModel classModel)
+        {
+            classModel.ClassId = Guid.NewGuid();
+
+            if (classModel != null)
+            {
+                Models.DBObjects.Class dbClassModel = new Models.DBObjects.Class();
+
+                dbClassModel.ClassId = classModel.ClassId;
+                dbClassModel.Name = classModel.Name;
+
+                dbContext.Classes.InsertOnSubmit(dbClassModel);
+                dbContext.SubmitChanges();
+            }
+        }
+        public List<ClassModel> GetAllClasses()
+        {
+            List<ClassModel> classesList = new List<ClassModel>();
+
+            foreach (Models.DBObjects.Class dbClassModel in dbContext.Classes)
+            {
+                ClassModel classModel = new ClassModel();
+
+                classModel.ClassId = dbClassModel.ClassId;
+                classModel.Name = dbClassModel.Name;
+
+                classesList.Add(classModel);
+            }
+            return classesList;
+        }
+        public ClassModel GetClassById(Guid id)
+        {
+            ClassModel classModel = new ClassModel();
+
+            Models.DBObjects.Class dbClassModel = dbContext.Classes.FirstOrDefault(m => m.ClassId == id);
+
+            if (dbClassModel != null)
+            {
+                classModel.ClassId = dbClassModel.ClassId;
+                classModel.Name = dbClassModel.Name;
+            }
+            return classModel;
+        }
+        public void Update(ClassModel classModel)
+        {
+            Models.DBObjects.Class dbClassModel = dbContext.Classes.FirstOrDefault(m => m.ClassId == classModel.ClassId);
+
+            if (classModel != null)
+            {
+                dbClassModel.ClassId = classModel.ClassId;
+                dbClassModel.Name = classModel.Name;
+
+                dbContext.SubmitChanges();
+            }
+        }
+        public void Delete(Guid id)
+        {
+            Models.DBObjects.Class dbClassModel = dbContext.Classes.FirstOrDefault(m => m.ClassId == id);
+
+            dbContext.Classes.DeleteOnSubmit(dbClassModel);
+            dbContext.SubmitChanges();
+        }
+    }
+}
