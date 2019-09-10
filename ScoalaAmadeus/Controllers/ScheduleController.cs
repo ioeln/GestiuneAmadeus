59d05892127fb43b38e2bc3b704815fe3dfb80a8
Nechita Ioel <ioelnechita@ymail.com>
2019-09-10 16:37:54 +0300

    Schedule: Controller , Repository and Views

---
+++ ScoalaAmadeus/Controllers/ScheduleController.cs
@@ -0,0 +1,127 @@
+﻿using ScoalaAmadeus.Models;
+using ScoalaAmadeus.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScoalaAmadeus.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private ScheduleRepository scheduleRepository = new ScheduleRepository();
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            List<ScheduleModel> schedulesList = new List<ScheduleModel>();
+            schedulesList = scheduleRepository.GetAllSchedules();
+            return View("Index", schedulesList);
+        }
+
+        // GET: Schedule/Details/5
+        public ActionResult Details(Guid id)
+        {
+            ScheduleModel scheduleModel = new ScheduleModel();
+            scheduleModel = scheduleRepository.GetScheduleById(id);
+            return View("Details", scheduleModel);
+        }
+
+        private StudentRepository studentRepository = new StudentRepository();
+        private TeacherRepository teacherRepository = new TeacherRepository();
+        private ClassRepository classRepository = new ClassRepository();
+
+        // GET: Schedule/Create
+        public ActionResult Create()
+        {
+            var students = studentRepository.GetAllStudents();
+            SelectList studentList = new SelectList(students, "StudentId", "Name");
+            ViewData["student1"] = studentList;
+
+            var teachers = teacherRepository.GetAllTeachers();
+            SelectList teacherList = new SelectList(teachers, "TeacherId", "Name");
+            ViewData["teacher1"] = teacherList;
+
+            var classes = classRepository.GetAllClasses();
+            SelectList classList = new SelectList(classes, "ClassId", "Name");
+            ViewData["class1"] = classList;
+
+            return View("Create");
+        }
+
+      
+        // POST: Schedule/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ScheduleModel scheduleModel = new ScheduleModel();
+
+                UpdateModel(scheduleModel);
+
+                scheduleRepository.Insert(scheduleModel);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+
+        // GET: Schedule/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            ScheduleModel scheduleModel = new ScheduleModel();
+            scheduleModel = scheduleRepository.GetScheduleById(id);
+            return View("Edit", scheduleModel);
+        }
+
+        // POST: Schedule/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Guid id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                ScheduleModel scheduleModel = new ScheduleModel();
+
+                UpdateModel(scheduleModel);
+
+                scheduleRepository.Update(scheduleModel);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Edit");
+            }
+        }
+
+        // GET: Schedule/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            ScheduleModel scheduleModel = new ScheduleModel();
+            scheduleModel = scheduleRepository.GetScheduleById(id);
+            return View("Delete", scheduleModel);
+        }
+
+        // POST: Schedule/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Guid id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                scheduleRepository.Delete(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("Delete");
+            }
+        }
+    }
+}
---
+++ ScoalaAmadeus/Repository/ScheduleRepository.cs
@@ -0,0 +1,99 @@
+﻿using ScoalaAmadeus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoalaAmadeus.Repository
+{
+    public class ScheduleRepository
+    {
+        private Models.DBObjects.SchoolsModelsDataContext dbContext;
+
+        public ScheduleRepository()
+        {
+            dbContext = new Models.DBObjects.SchoolsModelsDataContext();
+        }
+        public ScheduleRepository(Models.DBObjects.SchoolsModelsDataContext dataContext)
+        {
+            dbContext = dataContext;
+        }
+
+        public void Insert(ScheduleModel scheduleModel)
+        {
+            scheduleModel.ScheduleId = Guid.NewGuid();
+
+            if (scheduleModel != null)
+            {
+                Models.DBObjects.Schedule dbScheduleModel = new Models.DBObjects.Schedule();
+
+                dbScheduleModel.ScheduleId = scheduleModel.ScheduleId;
+                dbScheduleModel.Day = scheduleModel.Day;
+                dbScheduleModel.StudentId = scheduleModel.StudentId;
+                dbScheduleModel.TeacherId = scheduleModel.TeacherId;
+                dbScheduleModel.ClassId = scheduleModel.ClassId;
+
+                dbContext.Schedules.InsertOnSubmit(dbScheduleModel);
+                dbContext.SubmitChanges();
+            }
+        }
+        public List<ScheduleModel> GetAllSchedules()
+        {
+            List<ScheduleModel> schedulesList = new List<ScheduleModel>();
+
+            foreach (Models.DBObjects.Schedule dbScheduleModel in dbContext.Schedules)
+            {
+                ScheduleModel scheduleModel = new ScheduleModel();
+
+                scheduleModel.ScheduleId = dbScheduleModel.ScheduleId;
+                scheduleModel.Day = dbScheduleModel.Day;
+                scheduleModel.StudentId = dbScheduleModel.StudentId;
+                scheduleModel.TeacherId = dbScheduleModel.TeacherId;
+                scheduleModel.ClassId = dbScheduleModel.ClassId;
+
+                schedulesList.Add(scheduleModel);
+            }
+            return schedulesList;
+        }
+        public ScheduleModel GetScheduleById(Guid id)
+        {
+            Models.DBObjects.Schedule dbScheduleModel = dbContext.Schedules.FirstOrDefault(m => m.ScheduleId == id);
+
+            ScheduleModel scheduleModel = new ScheduleModel();
+
+            if (dbScheduleModel != null)
+            {
+                scheduleModel.ScheduleId = dbScheduleModel.ScheduleId;
+                scheduleModel.Day = dbScheduleModel.Day;
+                scheduleModel.StudentId = dbScheduleModel.StudentId;
+                scheduleModel.TeacherId = dbScheduleModel.TeacherId;
+                scheduleModel.ClassId = dbScheduleModel.ClassId;
+            }
+            return scheduleModel;
+        }
+
+        public void Update(ScheduleModel scheduleModel)
+        {
+            Models.DBObjects.Schedule dbScheduleModel = dbContext.Schedules.FirstOrDefault(m => m.ScheduleId == scheduleModel.ScheduleId);
+
+            if (scheduleModel != null)
+            {
+                dbScheduleModel.ScheduleId = scheduleModel.ScheduleId;
+                dbScheduleModel.Day = scheduleModel.Day;
+                dbScheduleModel.StudentId = scheduleModel.StudentId;
+                dbScheduleModel.TeacherId = scheduleModel.TeacherId;
+                dbScheduleModel.ClassId = scheduleModel.ClassId;
+
+                dbContext.SubmitChanges();
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            Models.DBObjects.Schedule dbScheduleModel = dbContext.Schedules.FirstOrDefault(m => m.ScheduleId == id);
+
+            dbContext.Schedules.DeleteOnSubmit(dbScheduleModel);
+            dbContext.SubmitChanges();
+        }
+    }
+}
