2416093dd232243b5d4cca2107b20c2f0e980c5d
Nechita Ioel <ioelnechita@ymail.com>
2019-10-01 21:00:26 +0300

    sad

--- ScoalaAmadeus/Controllers/InvoiceController.cs
+++ ScoalaAmadeus/Controllers/InvoiceController.cs
@@ -9,12 +9,15 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net.Mail;
 using System.Net;
+using System.IO;
+using System.IO.Compression;
 
 namespace ScoalaAmadeus.Controllers
 {
     
     public class InvoiceController : Controller
     {
+
         private InvoiceRepository invoiceRepository = new InvoiceRepository();
 
         // GET: Invoice
@@ -142,26 +145,151 @@ namespace ScoalaAmadeus.Controllers
 
         public ActionResult ExportPdf(int id)
         {
+            
+            string invoiceFileName = "";
             PreviewInvoiceViewModel exportModel = invoiceRepository.GetInvoicePreviewById(id);
 
-            return new ActionAsPdf("Export",  new { id = exportModel.InvoiceId })
+            var invoice = invoiceRepository.GetInvoiceById(id); 
+
+            var students = studentRepository.GetAllStudents();
+            foreach (var student in students)
             {
-                FileName = Server.MapPath("~/Content/Invoice.pdf")
+                if (student.StudentId == invoice.StudentId)
+                {
+                    invoiceFileName = student.Name;
+                }
+            }
+
+            return new ActionAsPdf("Export", new { id = exportModel.InvoiceId })
+            {
+                FileName = $"F-{invoice.InvoiceId}_{invoiceFileName}.pdf",
+                PageSize = Rotativa.Options.Size.A4,
+                PageOrientation = Rotativa.Options.Orientation.Portrait,
+                PageMargins = { Left = 1, Right = 1 }
             };
-          
         }
+
+        public ActionResult SaveInvoice(int id)
+        {
+            string invoiceFileName = "";
+            PreviewInvoiceViewModel exportModel = invoiceRepository.GetInvoicePreviewById(id);
+
+            var invoice = invoiceRepository.GetInvoiceById(id);
+
+            var students = studentRepository.GetAllStudents();
+            foreach (var student in students)
+            {
+                if (student.StudentId == invoice.StudentId)
+                {
+                    invoiceFileName = student.Name;
+                }
+            }
+            try
+            {
+                SaveHttpResponseAsFile("http://localhost:49786/Invoice/ExportPdf/?id=" + id, $"C:\\TEMP\\F-{invoice.InvoiceId}_{invoiceFileName}.pdf");
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("SaveInvoice");
+               
+            }
+        }
+        // PDF generator for email
+        public static void SaveHttpResponseAsFile(string RequestUrl, string FilePath)
+        {
+            try
+            {
+                HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(RequestUrl);
+                httpRequest.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows 10 Pro; Trident/5.0)";
+                httpRequest.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+                HttpWebResponse response = null;
+                try
+                {
+                    response = (HttpWebResponse)httpRequest.GetResponse();
+                }
+                catch (System.Net.WebException ex)
+                {
+                    if (ex.Status == WebExceptionStatus.ProtocolError)
+                        response = (HttpWebResponse)ex.Response;
+                }
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    Stream FinalStream = responseStream;
+                    if (response.ContentEncoding.ToLower().Contains("gzip"))
+                        FinalStream = new GZipStream(FinalStream, CompressionMode.Decompress);
+                    else if (response.ContentEncoding.ToLower().Contains("deflate"))
+                        FinalStream = new DeflateStream(FinalStream, CompressionMode.Decompress);
+
+                    using (var fileStream = System.IO.File.Create(FilePath))
+                    {
+                        FinalStream.CopyTo(fileStream);
+                    }
+
+                    response.Close();
+                    FinalStream.Close();
+                }
+            }
+            catch
+            { }
+        }
+
+        
+
+        public Byte[] SaveInvoiceMail(int id)
+        {
+            string invoiceFileName = "";
+            PreviewInvoiceViewModel exportModel = invoiceRepository.GetInvoicePreviewById(id);
+
+            var invoice = invoiceRepository.GetInvoiceById(id);
+
+            var students = studentRepository.GetAllStudents();
+            foreach (var student in students)
+            {
+                if (student.StudentId == invoice.StudentId)
+                {
+                    invoiceFileName = student.Name;
+                }
+            }
+           
+            var pdf = new ActionAsPdf("Export", new { id = exportModel.InvoiceId })
+            {
+                FileName = $"F-{invoice.InvoiceId}_{invoiceFileName}.pdf",
+            };
+
+            Byte[] pdfData = pdf.BuildPdf(ControllerContext);
+            return pdfData;
+        }
+
 
         public ActionResult SendEmail()
         {
-
             return View();
         }
 
         [HttpPost]
         public ActionResult SendEmail(string receiver, string subject, string message)
         {
+            //string receiver = "ioelnechita@ymail.com";
+            //string subject = "Test";
+            //string message = "Acesta este mesajul testului cu pdf attach";
+            int id = 32;
+            string invoiceFileName = "";
+            var invoice = invoiceRepository.GetInvoiceById(id);
+
+            var students = studentRepository.GetAllStudents();
+            foreach (var student in students)
+            {
+                if (student.StudentId == invoice.StudentId)
+                {
+                    invoiceFileName = student.Name;
+                }
+            }
             try
             {
+                
+
                 if (ModelState.IsValid)
                 {
                     var senderEmail = new MailAddress("scoala.conta123@gmail.com", "emailScoala");
@@ -169,6 +297,11 @@ namespace ScoalaAmadeus.Controllers
                     var password = "scoala#123456";
                     var sub = subject;
                     var body = message;
+                    MemoryStream stream = new MemoryStream(SaveInvoiceMail(id));
+                    Attachment att1 = new Attachment(stream, $"F-{invoice.InvoiceId}_{invoiceFileName}.pdf", "application/pdf");
+                    
+
+
                     var smtp = new SmtpClient
                     {
                         Host = "smtp.gmail.com",
@@ -181,7 +314,10 @@ namespace ScoalaAmadeus.Controllers
                     using (var mess = new MailMessage(senderEmail, receiverEmail)
                     {
                         Subject = subject,
-                        Body = body
+                        Body = body,
+                        //Attachments = new AttachmentCollection().Add(att1);
+                      
+                        //Attachments = 
                     })
                     {
                         smtp.Send(mess);
